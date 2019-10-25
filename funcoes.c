0567a37f7a9cab3bab88cead474cc08f82cafc62
CAPPA2ZZ <paulocapa55@gmail.com>
2019-10-25 13:56:35 +0100

    Comentarios

--- funcoes.c
+++ funcoes.c
@@ -14,13 +14,14 @@ typedef struct Film
 //Insere um filme novo na lista
 Film* insertFilm(Film *head , int id, char title[N])
 {
-    Film* new = (Film*) malloc (sizeof(Film));
-    new->id = id;
-    strcpy(new->title,title);
-    new->next = head;
-    return new;
+    Film* new = (Film*) malloc (sizeof(Film));  //Alocar a memoria
+    new->id = id;//Copiar o id para o no elemento
+    strcpy(new->title,title);//Copiar o titulo
+    new->next = head;//Por no topo da lista o novo filme
+    return new;//Retorna a nova head da lista 
 }
 
+//Escreve todos os filmes na lista no ecrã
 void printFilm(Film *head)
 {
     Film *a = head;
@@ -32,6 +33,7 @@ void printFilm(Film *head)
     //printf("Size: %lu", sizeof(Film));
 }
 
+//Recebe o id do filme e o apontador para a lista de filmes e escreve no ecrã o titulo do filme
 void title(int id ,Film* head)
 {
     while(head != NULL)
